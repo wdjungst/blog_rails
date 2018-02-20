5abe8da67a13d02c5d43e030bfa56f810e118b9a
David Jungst <djungst@gmail.com>
2018-02-19 20:07:39 -0700

    page creation

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,33 @@
+class PagesController < ApplicationController
+  def index
+    @pages = Page.all
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(page_params)
+    if @page.save
+      redirect_to page_path(@page)
+    else
+      render :new
+    end
+  end
+
+  private
+    def page_params
+      params.require(:page).permit(:author, :title, :body)
+    end
+
+end
+
+
+
+
+
