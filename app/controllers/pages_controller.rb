4cc879049339c96ed239e159689c143aee965e3b
David Jungst <djungst@gmail.com>
2018-02-19 20:39:05 -0700

    update and destroy pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,6 +20,25 @@ class PagesController < ApplicationController
     end
   end
 
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to page_path(@page)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
+  end
+
   private
     def page_params
       params.require(:page).permit(:author, :title, :body)
